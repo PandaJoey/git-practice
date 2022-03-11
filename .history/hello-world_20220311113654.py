fd3b6ed112f2b509b0164b552629fece050fed1d
PandaJoey <pandajoey77@gmail.com>
2022-03-11 11:38:08 +0000

    added line to commit

--- .history/hello-world_20220311113654.py
+++ .history/hello-world_20220311113654.py
@@ -1,8 +1,6 @@
 class HelloWorld:
     def hello_world(self):
         print("Hello, World!")
-        print("added to first branch")
-        print("adding line to commit")
         
 hello = HelloWorld()
 hello.hello_world()
